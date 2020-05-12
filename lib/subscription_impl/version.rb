40d12efcef8cf8731672fad168ad7c63b8695f85
Bradley J. Tannor <bradleytannor@gmail.com>
2020-05-12 13:03:46 -0700

    Prepare Deployment
    Fixed missing 'require' statement.

--- lib/subscription_impl/version.rb
+++ lib/subscription_impl/version.rb
@@ -1,6 +1,8 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
 # under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
+require 'subscription_int'
+
 # Subscription.
 # @class_description
 #   A Subscription library implementation.
